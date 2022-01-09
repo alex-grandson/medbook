4cb672a51d8d6f87c7f283940c9d2d98135976a8
alex-grandson <285484@niuitmo.ru>
2022-01-10 02:32:50 +0300

    поправил константу тайм периода

--- src/components/schedule/AppointmentEvent.js
+++ src/components/schedule/AppointmentEvent.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import { Divider, Stack, Typography } from '@mui/material';
-
-const timePeriod = {
-  1: '9:00',
-  2: '9:30',
-  3: '10:00',
-  4: '10:30',
-  5: '11:00',
-  6: '11:30',
-  7: '12:00',
-  8: '12:30',
-  9: '13:00',
-  10: '13:30',
-  11: '14:00',
-  12: '14:30',
-  13: '15:00',
-  14: '15:30',
-  15: '16:00',
-  16: '16:30',
-  17: '17:00',
-  18: '17:30',
-  19: '18:00',
-  20: '18:30',
-  21: '19:00'
-};
+import { timePeriod } from '../../constants';
 
 export default function AppointmentEvent({ event }) {
   return (
