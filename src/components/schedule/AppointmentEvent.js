ad137d6a31096c9867171aef891758db03b2d47e
alex-grandson <285484@niuitmo.ru>
2022-01-20 04:40:30 +0300

    appointment event coloring

--- src/components/schedule/AppointmentEvent.js
+++ src/components/schedule/AppointmentEvent.js
@@ -7,7 +7,6 @@ import MarkAppointmentDialog from './MarkAppointmentDialog';
 export default function AppointmentEvent(props) {
   const { slot, setSelectedUser } = props;
   const { data, isLoading } = useGetUsersQuery(slot.patientId);
-
   const customStackStyles = {
     display: 'flex',
     flexDirection: 'row',
@@ -28,7 +27,15 @@ export default function AppointmentEvent(props) {
         />
       )}
       <Divider />
-      <Stack direction="row" style={{ padding: '20px 10px' }}>
+      <Stack
+        direction="row"
+        style={{
+          padding: '20px 10px',
+          backgroundColor: slot.accepted ? 'rgba(133, 255, 151, 0.4)' : 'transparent',
+          margin: '10px 0',
+          borderRadius: '20px'
+        }}
+      >
         <Stack direction="column" style={{ textAlign: 'center', opacity: 0.75, marginRight: 15 }}>
           <Typography variant="p">{TIME_PERIOD[slot.timeSlot]}</Typography>
           <div style={{ margin: '-7px 0' }}>-</div>
@@ -42,15 +49,17 @@ export default function AppointmentEvent(props) {
             <Typography variant="p">{slot.comment ? slot.comment : 'Комментарий'}</Typography>
           </div>
 
-          <Button
-            style={{ marginLeft: '16px' }}
-            onClick={() => {
-              setShowModal(true);
-              setSelectedUser(slot.patientId);
-            }}
-          >
-            Отметить
-          </Button>
+          {!slot.accepted && (
+            <Button
+              style={{ marginLeft: '16px' }}
+              onClick={() => {
+                setShowModal(true);
+                setSelectedUser(slot.patientId);
+              }}
+            >
+              Отметить
+            </Button>
+          )}
         </Stack>
       </Stack>
     </>
